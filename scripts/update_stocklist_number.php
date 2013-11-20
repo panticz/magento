96e6cdc51adc26fe65fa3422bd4d0926556448d5
panticz <mail@konczalski.de>
2013-11-20 14:16:38 +0100

    Rewrite

--- scripts/update_stocklist_number.php
+++ scripts/update_stocklist_number.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php
 <?php
+require '/var/www/app/Mage.php';
+Mage::app(0);
+  
 if(count($_SERVER['argv']) > 1) {
   $value=$_SERVER['argv'][1];
 
-  require '/var/www/app/Mage.php';
-  Mage::app(0);
-
+  # update config
   $config = new Mage_Core_Model_Config();
   $config->saveConfig('stocklist/number', $value, 'default', 0);
 
+  # refresh configuration cache
   Mage::app()->getCacheInstance()->cleanType('config');
 } else {
   echo "Usage: " . $_SERVER['argv'][0] . " LIST_ID\n";
