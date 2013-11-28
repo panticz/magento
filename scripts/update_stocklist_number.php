461a351cf5cc0e6a7d4f09aca4171621636cbf68
panticz <mail@konczalski.de>
2013-11-28 22:01:42 +0100

    Rewrite comments

--- scripts/update_stocklist_number.php
+++ scripts/update_stocklist_number.php
@@ -6,11 +6,11 @@ Mage::app(0);
 if(count($_SERVER['argv']) == 2) {
   $value = $_SERVER['argv'][1];
 
-  # update config
+  # update magento core config data
   $config = new Mage_Core_Model_Config();
   $config->saveConfig('stocklist/number', $value, 'default', 0);
 
-  # refresh configuration cache
+  # refresh magento configuration cache
   Mage::app()->getCacheInstance()->cleanType('config');
 } else {
   echo "Usage: " . $_SERVER['argv'][0] . " LIST_ID\n";
